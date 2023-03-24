580712323136110fb3454af17f6b95d9ab87c920
Jeric Nisperos <jericrnisperos@gmail.com>
2023-03-24 23:09:14 +0800

    Added new Icons to Navbar

--- minecommunity/pages/components/Navbar.jsx
+++ minecommunity/pages/components/Navbar.jsx
@@ -4,7 +4,8 @@ import { useRouter } from "next/router";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDiscord } from "@fortawesome/free-brands-svg-icons";
+import { faDiscord, faGoodreads } from "@fortawesome/free-brands-svg-icons";
+import { faGamepad, faHouse, faPeopleGroup, faShop } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   const router = useRouter();
@@ -13,7 +14,8 @@ function Navbar() {
     <div>
       <nav className="w-full bg-gray-800 shadow">
         <div className="justify-between px-4 mx-auto lg:max-w-5 md:items-center md:flex md:px-8">
-          <div alt="Icon and Name"
+          <div
+            alt="Icon and Name"
             className="flex items-center justify-between py-3 md:py-5 md:block flex-no-wrap"
           >
             <motion.div
@@ -49,30 +51,31 @@ function Navbar() {
                 className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 `}
               >
                 <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                  <motion.div
+                  {/* <motion.div
                     whileHover={{
                       scale: 1.1,
                       transition: { duration: 0.5 },
                     }}
                     transition={{ type: "spring", stiffness: 200, damping: 10 }}
-                  >
+                  > */}
                     <li
-                      className={`text-white font-extrabold underline-offset-0 px-3 py-2 ${
+                      className={`text-white font-extrabold  px-3 py-2 ${
                         router.pathname === "/"
                           ? "bg-blue-500  rounded-md border-solid border-2 border-blue-500"
                           : " "
                       }`}
                     >
-                      <Link href="/">HOME</Link>
+                        <FontAwesomeIcon icon={faHouse} className="text-md"/>
+                      <Link href="/">  HOME</Link>
                     </li>
-                  </motion.div>
-                  <motion.div
+                  {/* </motion.div> */}
+                  {/* <motion.div
                     whileHover={{
                       scale: 1.1,
                       transition: { duration: 0.5 },
                     }}
                     transition={{ type: "spring", stiffness: 200, damping: 10 }}
-                  >
+                  > */}
                     <li
                       className={`text-white font-extrabold  px-3 py-2 ${
                         router.pathname === "/shop"
@@ -80,16 +83,17 @@ function Navbar() {
                           : " "
                       }`}
                     >
-                      <Link href="/shop">SHOP</Link>
+                        <FontAwesomeIcon icon={faShop} className="text-md"/>
+                      <Link href="/shop">  SHOP</Link>
                     </li>
-                  </motion.div>
-                  <motion.div
+                  {/* </motion.div> */}
+                  {/* <motion.div
                     whileHover={{
                       scale: 1.1,
                       transition: { duration: 0.5 },
                     }}
                     transition={{ type: "spring", stiffness: 200, damping: 10 }}
-                  >
+                  > */}
                     <li
                       className={`text-white font-extrabold  px-3 py-2 ${
                         router.pathname === "/guides"
@@ -97,16 +101,18 @@ function Navbar() {
                           : " "
                       }`}
                     >
-                      <Link href="/guides">GUIDES</Link>
+                    <FontAwesomeIcon icon={faGamepad} className="text-md"/>
+                      <Link href="/guides">  GUIDES</Link>
+                      
                     </li>
-                  </motion.div>
-                  <motion.div
+                  {/* </motion.div> */}
+                  {/* <motion.div
                     whileHover={{
                       scale: 1.1,
                       transition: { duration: 0.5 },
                     }}
                     transition={{ type: "spring", stiffness: 200, damping: 10 }}
-                  >
+                  > */}
                     <li
                       className={`text-white font-extrabold  px-3 py-2 ${
                         router.pathname === "/about"
@@ -114,19 +120,27 @@ function Navbar() {
                           : " "
                       }`}
                     >
-                      <Link href="/about">ABOUT US</Link>
+                        <FontAwesomeIcon icon={faPeopleGroup} className="text-md"/>
+                      <Link href="/about">  ABOUT US</Link>
                     </li>
-                  </motion.div>
+                  {/* </motion.div> */}
                 </ul>
               </div>
             </motion.div>
           </div>
           <div alt="discord tska IP">
-            <a className="text-white px-3 py-2 mx-3  hover:text-black bg-blue-300 text-xs">
-            <FontAwesomeIcon icon={faDiscord} size="xs" className="text-xs" />
+            <a className="text-white fa-xs hover:text-black">
+              {/* <FontAwesomeIcon icon={faDiscord} size="xs" /> */}
             </a>
-                <motion.a className="text-white font-extrabold px-3 py-2 bg-blue-500  rounded-md border-solid border-2 border-blue-500 cursor-pointer" onClick={()=> {IPAddress === "PLAY.MINE-SMP.ME" ? setIPAddress("📋 Copied!") : setIPAddress("PLAY.MINE-SMP.ME")}}>
-            {IPAddress}
+            <motion.a
+              className="text-white font-extrabold px-3 py-2 bg-blue-500  rounded-md border-solid border-2 border-blue-500 cursor-pointer"
+              onClick={() => {
+                IPAddress === "PLAY.MINE-SMP.ME"
+                  ? setIPAddress("📋 Copied!")
+                  : setIPAddress("PLAY.MINE-SMP.ME");
+              }}
+            >
+              {IPAddress}
             </motion.a>
           </div>
         </div>
