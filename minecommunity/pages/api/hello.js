ddd345097b1e8ce92bae471a983ee5642379f28a
Jeric Nisperos <jericrnisperos@gmail.com>
2023-03-25 21:37:29 +0800

    removed plan and fixed hello API

--- minecommunity/pages/api/hello.js
+++ minecommunity/pages/api/hello.js
@@ -1,5 +1,14 @@
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const response = await fetch('http://play.mine-smp.me:25571/v1/serverOverview?server=MineCommunity%20Guilds');
+  const data = await response.json();
+
+  res.json(data);
 }
