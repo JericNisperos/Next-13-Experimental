c382c30f486caa60b25fff65a783bc19d82b5ea6
Jeric Nisperos <jericrnisperos@gmail.com>
2023-03-25 10:38:37 +0800

    added to APP.js

--- minecommunity/pages/_app.js
+++ minecommunity/pages/_app.js
@@ -1,12 +1,18 @@
-import Navbar from './components/Navbar'
-import './globals.scss'
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import Navbar from "./components/Navbar";
+import "./globals.scss";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 // Prevent fontawesome from adding its CSS since we did it manually above:
-import { config } from '@fortawesome/fontawesome-svg-core';
+import { config } from "@fortawesome/fontawesome-svg-core";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <>
-  <Navbar />
-  <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
