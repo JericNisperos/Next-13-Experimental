5aaa8acd2de59aa27fa4efcdeaaebfa65e2d83bd
Jeric Nisperos <jericrnisperos@gmail.com>
2023-03-26 00:49:39 +0800

    added more items

--- minecommunity/pages/components/Navbar.jsx
+++ minecommunity/pages/components/Navbar.jsx
@@ -10,7 +10,6 @@ import MenuButton from "./MenuButton";
 import Head from "next/head";
 // import handler from "../api/hello";
 
-
 function Navbar() {
   const router = useRouter();
   const [IPAddress, setIPAddress] = useState(`PLAY.MINE-SMP.ME`);
@@ -37,71 +36,69 @@ function Navbar() {
   }
   return (
     <>
-    <Head>
-    {/* <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" /> */}
-</Head>
-<main>
-    <div>
-      <nav className="w-full shadow">
-        <div className="justify-between px-4 lg:max-w-5 md:items-center md:flex md:px-8 md:mx-48" >
-          <div alt="Icon and Name" className="flex items-center justify-between py-3 md:py-5 md:block flex-no-wrap">
-            <motion.div whileHover={{ scale: 1.1, transition: { duration: 0.5 } }} transition={{ type: "spring", stiffness: 200, damping: 10 }} className="flex items-center">
-              <Image src="https://media.discordapp.net/attachments/1083083786704658472/1088132326732607609/MineCommunity_Icon.png" width="48" height="48" alt="logo" className="flex-shrink-0 rounded-md" />
-              <Link href="/" className="text-2xl text-white font-bold ml-2">
-                MineCommunity
-              </Link>
-            </motion.div>
-            <button className="md:hidden px-3 py-2 text-white rounded-md outline-none focus:border-gray-400 focus:border">
-              <MenuButton isOpen={navMobile} onClick={() => setNavMobile(!navMobile)} className="md:hidden px-3 py-2 text-white rounded-md outline-none focus:border-gray-400 focus:border" />
-            </button>
-          </div>
-          <div alt="Navbar Options" className={` ${!navMobile ? "hidden md:block" : " "}`}>
-            <motion.div initial={{ opacity: 1, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>
-              <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 `}>
-                <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                  <NavItems href="/" icon={faHouse} title="HOME" />
-                  <NavItems href="/shop" icon={faShop} title="SHOP" />
-                  <NavItems href="/guides" icon={faGamepad} title="GUIDES" />
-                  <NavItems href="/about" icon={faPeopleGroup} title="ABOUT US" />
-                  <div className={`text-white items-center flex justify-between ${!navMobile ? "hidden" : " "}`}>
-                    <div className="bg-blue-500 px-4 py-2 items-center justify-between rounded-md cursor-pointer bg-discord">
-                      <FontAwesomeIcon icon={faDiscord} />
-                      &nbsp;Discord
-                    </div>
-                    <div>
-                      <motion.a
-                        className="text-white font-extrabold px-3 py-2 bg-blue-500  rounded-md border-solid border-2 border-blue-500 cursor-pointer"
-                        onClick={() => {
-                          navigator.clipboard.writeText("play.mine-smp.me");
-                          IPAddress === "PLAY.MINE-SMP.ME" ? setIPAddress("📋 Copied!") : setIPAddress("PLAY.MINE-SMP.ME");
-                        }}
-                      >
-                        {IPAddress}&nbsp; | <FontAwesomeIcon icon={faPerson} size="xs" /> {totalPlayers}
-                      </motion.a>
-                    </div>
-                  </div>
-                </ul>
+      <Head>{/* <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" /> */}</Head>
+      <main>
+        <div>
+          <nav className="w-full shadow">
+            <div className="justify-between px-4 lg:max-w-5 md:items-center md:flex md:px-8 md:mx-48">
+              <div alt="Icon and Name" className="flex items-center justify-between py-3 md:py-5 md:block flex-no-wrap">
+                <motion.div whileHover={{ scale: 1.1, transition: { duration: 0.5 } }} transition={{ type: "spring", stiffness: 200, damping: 10 }} className="flex items-center">
+                  <Image src="https://media.discordapp.net/attachments/1083083786704658472/1088132326732607609/MineCommunity_Icon.png" width="48" height="48" alt="logo" className="flex-shrink-0 rounded-md" />
+                  <Link href="/" className="text-2xl text-white font-bold ml-2">
+                    MineCommunity
+                  </Link>
+                </motion.div>
+                <button className="md:hidden px-3 py-2 text-white rounded-md outline-none focus:border-gray-400 focus:border">
+                  <MenuButton isOpen={navMobile} onClick={() => setNavMobile(!navMobile)} className="md:hidden px-3 py-2 text-white rounded-md outline-none focus:border-gray-400 focus:border" />
+                </button>
               </div>
-            </motion.div>
-          </div>
-          <div alt="discord tska IP" className="hidden md:block">
-            <a className="text-white mx-4 bg-discord px-3 py-2 rounded-lg cursor-pointer " href="https://discord.com/invite/minecommunity" target="_blank">
-              <FontAwesomeIcon icon={faDiscord} />
-            </a>
-            <motion.a
-              className="text-white font-extrabold px-3 py-2 bg-blue-500  rounded-md border-solid border-2 border-blue-500 cursor-pointer"
-              onClick={() => {
-                navigator.clipboard.writeText("play.mine-smp.me");
-                IPAddress === "PLAY.MINE-SMP.ME" ? setIPAddress("📋 Copied!") : setIPAddress("PLAY.MINE-SMP.ME");
-              }}
-            >
-              {IPAddress}&nbsp; | <FontAwesomeIcon icon={faPerson} size="xs" /> {totalPlayers}
-            </motion.a>
-          </div>
+              <div alt="Navbar Options" className={` ${!navMobile ? "hidden md:block" : " "}`}>
+                <motion.div initial={{ opacity: 1, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>
+                  <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 `}>
+                    <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                      <NavItems href="/" icon={faHouse} title="HOME" />
+                      <NavItems href="/shop" icon={faShop} title="SHOP" />
+                      <NavItems href="/guides" icon={faGamepad} title="GUIDES" />
+                      <NavItems href="/about" icon={faPeopleGroup} title="ABOUT US" />
+                      <div className={`text-white items-center flex justify-between ${!navMobile ? "hidden" : " "}`}>
+                        <div className="bg-blue-500 px-4 py-2 items-center justify-between rounded-md cursor-pointer bg-discord">
+                          <FontAwesomeIcon icon={faDiscord} />
+                          &nbsp;Discord
+                        </div>
+                        <div>
+                          <motion.a
+                            className="text-white font-extrabold px-3 py-2 bg-blue-500  rounded-md border-solid border-2 border-blue-500 cursor-pointer"
+                            onClick={() => {
+                              navigator.clipboard.writeText("play.mine-smp.me");
+                              IPAddress === "PLAY.MINE-SMP.ME" ? setIPAddress("📋 Copied!") : setIPAddress("PLAY.MINE-SMP.ME");
+                            }}
+                          >
+                            {IPAddress}&nbsp; | <FontAwesomeIcon icon={faPerson} size="xs" /> {totalPlayers}
+                          </motion.a>
+                        </div>
+                      </div>
+                    </ul>
+                  </div>
+                </motion.div>
+              </div>
+              <div alt="discord tska IP" className="hidden md:block">
+                <a className="text-white mx-4 bg-discord px-3 py-2 rounded-lg cursor-pointer " href="https://discord.com/invite/minecommunity" target="_blank">
+                  <FontAwesomeIcon icon={faDiscord} />
+                </a>
+                <motion.a
+                  className="text-white font-extrabold px-3 py-2 bg-blue-500  rounded-md border-solid border-2 border-blue-500 cursor-pointer"
+                  onClick={() => {
+                    navigator.clipboard.writeText("play.mine-smp.me");
+                    IPAddress === "PLAY.MINE-SMP.ME" ? setIPAddress("📋 Copied!") : setIPAddress("PLAY.MINE-SMP.ME");
+                  }}
+                >
+                  {IPAddress}&nbsp; | <FontAwesomeIcon icon={faPerson} size="xs" /> {totalPlayers}
+                </motion.a>
+              </div>
+            </div>
+          </nav>
         </div>
-      </nav>
-    </div>
-    </main>
+      </main>
     </>
   );
 }
--- minecommunity/pages/index.js
+++ minecommunity/pages/index.js
@@ -16,10 +16,15 @@ export default function Home() {
       </Head>
       <main>
         {/* <Home /> */}
-        <motion.div className="text-white">
-          <Image className="vignette justify-center items-center flex" src="https://media.discordapp.net/attachments/1083083786704658472/1083083816475840512/wwwww.png?width=1084&height=610" alt="cover" width="1084" height="610"/>
-          </motion.div>
+        <motion.div className="text-white mx-auto justify-center items-center flex flex-nowrap rounded-md" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+          <div>
+            <Image className="rounded-md" src="https://media.discordapp.net/attachments/1083083786704658472/1083083816475840512/wwwww.png?width=1084&height=610" alt="cover" width="1084" height="610" />
+          </div>
 
+        </motion.div>
+        <motion.div initial={{ opacity: 0, y: 15 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} className="text-white mx-auto justify-center items-center flex text-2xl font-proxima font-extrabold mt-5">
+            <p>Mining Creativity, Building Community</p>
+          </motion.div>
       </main>
     </>
   );
